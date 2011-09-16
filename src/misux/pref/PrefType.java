8ecc17396bd64e4af26364252939fe734d579803
DSIW <dsiw@privatdemail.net>
2011-09-16 12:19:39 +0200

    false import

--- src/misux/pref/PrefType.java
+++ src/misux/pref/PrefType.java
@@ -1,6 +1,6 @@
 /*
  * misux - musicplayer (written in Java)
-Copyright (C) 2011  DSIW <dsiw@privatdemail.net>
+ * Copyright (C) 2011  DSIW <dsiw@privatdemail.net>
  * 
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
  * 
